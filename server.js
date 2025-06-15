6725cc414cb7c6ab302834472a41aa1fa4f3be67
abhishek <abhishekksharmaa11@gmail.com>
2025-06-15 20:13:15 +0530

    dropdown option apis

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require("./routes/authRoutes");
 const astrologerRoutes = require('./routes/astrologerRoutes'); //spellcheck
+const dropdownRoutes = require('./routes/dropdownRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use('/api/astrologer', astrologerRoutes);
+app.use('/api', dropdownRoutes); 
 // const authRoutes = require('./routes/auth');
 // app.use('/api', authRoutes);
 
