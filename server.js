e59bdb5d171735f8df2d76a6a705909a4f3c6e7d
abhishek <abhishekksharmaa11@gmail.com>
2025-06-15 12:00:45 +0530

    port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const astrologerRoutes = require('./routes/astrologerRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 2025;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
