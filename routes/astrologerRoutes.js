6e615af6ab2c6f5ceb291ad0f6a22da2d6025441
abhishek <abhishekksharmaa11@gmail.com>
2025-06-15 17:20:04 +0530

    test7

--- routes/astrologerRoutes.js
+++ routes/astrologerRoutes.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../utilis/cloudinary'); // your multer config vv
+const { upload } = require('../utilis/cloudinary');  // your multer config
 const astrologerController = require('../controllers/astrologerController');
 
 router.post('/register', upload.single('user_profile'), astrologerController.registerAstrologer);
-router.get('/getAllAstrologer', astrologerController.getAllAstrologers);
+router.get('/all', astrologerController.getAllAstrologers);
 router.get('/dropdowns', astrologerController.getDropdownOptions);
 router.put('/update/:id', upload.single('user_profile'), astrologerController.updateAstrologer);
 router.delete('/delete/:id', astrologerController.deleteAstrologer);
 
 module.exports = router;
-
-
-
-
-s
-//--------//
