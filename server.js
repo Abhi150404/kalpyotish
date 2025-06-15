706e0cc4e2f445c49843ec7ad63fe8a55e59003e
abhishek <abhishekksharmaa11@gmail.com>
2025-06-15 12:10:21 +0530

    spellchecked

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require("./routes/authRoutes");
-const astrologerRoutes = require('./routes/astrologerRoutes');
+const astrologerRoutes = require('./routes/astrologerRoutes'); //spellcheck
 
 const app = express();
 const PORT = process.env.PORT || 5000;
