b7948b9f0706510b39401e425d6c7af98a131d93
abhishek <abhishekksharmaa11@gmail.com>
2025-06-16 23:11:14 +0530

    patch

--- routes/transitRoutes.js
+++ routes/transitRoutes.js
@@ -6,7 +6,7 @@ const { upload } = require('../utilis/cloudinary'); // Adjust path as needed
 router.post('/add-trasnsit', upload.single('image'), transitController.createTransit);
 router.get('/get-trasnsit', transitController.getAllTransits);
 router.get('/update-trasnsit/:id', transitController.getTransitById);
-router.patch('update-image/:id', upload.single('image'), transitController.updateTransit);
+router.patch('/update-trasnsit/:id', upload.single('image'), transitController.updateTransit);
 router.delete('/delete-trasnsit/:id', transitController.deleteTransit);
 
 module.exports = router;
