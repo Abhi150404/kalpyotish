c1ee7f94e0b830d2d6b2ec6cbedb67d766f3a6c3
abhishek <abhishekksharmaa11@gmail.com>
2025-06-24 22:41:53 +0530

    registerapis

--- routes/astrologerRoutes.js
+++ routes/astrologerRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { upload } = require('../utilis/cloudinary');  // your multer config
 const astrologerController = require('../controllers/astrologerController');
 
-router.post('/register', upload.single('profilePhoto'), registerAstrologer);
+router.post('/register', upload.single('profilePhoto'),  astrologerController.registerAstrologer);
 
 // Update profile photo
-router.patch('/update-profile/:id', upload.single('profilePhoto'), updateProfilePhoto);
+router.patch('/update-profile/:id', upload.single('profilePhoto'),  astrologerController.updateProfilePhoto);
 
 router.get('/all', astrologerController.getAllAstrologers);
 router.get('/dropdowns', astrologerController.getDropdownOptions);
