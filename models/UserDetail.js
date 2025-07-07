f82ab5ceffd6c9effda0f45cb9ec53b56ad98139
developerxx <sagarbisht07x@gmail.com>
2025-07-07 21:47:33 +0530

    modal update

--- models/UserDetail.js
+++ models/UserDetail.js
@@ -10,4 +10,4 @@ const userSchema = new mongoose.Schema({
   profile: String, // Cloudinary URL
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('UserDetail', userSchema, 'UserDetail');
