354cbd107f6a38c8e7a8a0dd99b9ac004c06747a
abhishek <abhishekksharmaa11@gmail.com>
2025-09-10 23:14:16 +0530

    test

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { saveFcmToken, updateFcmToken } = require("../controllers/notificationController");
+const { saveFcmToken, updateFcmToken,  createNotification,
+  getNotifications, } = require("../controllers/notificationController");
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ router.post("/save-token", saveFcmToken);
 
 // Update existing token
 router.put("/update-token", updateFcmToken);
+
+router.post("/create", createNotification);
+
+// Fetch all or by user/astrologer id
+router.get("/list", getNotifications);
 
 module.exports = router;
