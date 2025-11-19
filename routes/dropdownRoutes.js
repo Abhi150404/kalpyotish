53b89db3a09beaa57bfcc6d574e51b8ca135a722
abhishek <abhishekksharmaa11@gmail.com>
2025-11-19 22:54:51 +0530

    okk

--- routes/dropdownRoutes.js
+++ routes/dropdownRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dropdownCtrl = require('../controllers/dropdownController');
 
-router.get('/dropdowns', dropdownCtrl.getDropdownOptions);
+router.get('/astro_skills', dropdownCtrl.getDropdownOptions);
 router.post('/new-dropdowns', dropdownCtrl.addDropdownItem);
 router.put('/update-dropdowns/:id', dropdownCtrl.updateDropdownItem);
 router.delete('/delete-dropdowns/:id', dropdownCtrl.deleteDropdownItem);
