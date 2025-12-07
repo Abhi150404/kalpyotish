a54d016f3aa174abceacddce48bfba562b90bc7c
abhishek <abhishekksharmaa11@gmail.com>
2025-12-07 23:18:08 +0530

    neww

--- config/fcm.js
+++ config/fcm.js
@@ -1,9 +1,15 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../kalpjyotish-d1f52-firebase-adminsdk-fbsvc-94699fae64.json");
+const path = require("path");
+
+// Load Firebase service account JSON securely
+const serviceAccountPath = path.join(
+  __dirname,
+  "keys",
+  "kalpjyotish-d1f52-firebase-adminsdk-fbsvc-94699fae64.json"
+);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(require(serviceAccountPath)),
 });
-
 
 module.exports = admin;
