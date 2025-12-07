5e57c5b30c5ae75db62554fac139cf2c624a9e46
abhishek <abhishekksharmaa11@gmail.com>
2025-12-07 22:32:43 +0530

    newfile

--- config/fcm.js
+++ config/fcm.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../kapljyotish.json");
+const serviceAccount = require("../kalpjyotish-d1f52-firebase-adminsdk-fbsvc-94699fae64.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
