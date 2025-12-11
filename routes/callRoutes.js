245c1e422856c3053a33aea39c0f5bfef130d954
abhishek <abhishekksharmaa11@gmail.com>
2025-12-11 21:10:14 +0530

    new

--- routes/callRoutes.js
+++ routes/callRoutes.js
@@ -4,6 +4,6 @@ const CallController = require("../controllers/callController");
 router.post("/start", CallController.startCall);
 router.post("/update-status", CallController.updateCallStatus);
 router.get("/history/:userId", CallController.getCallHistory);
-router.get("/earnings",CallController.getEarnings);
+router.get("/earnings/:userId",CallController.getEarnings);
 
 module.exports = router;
